1b88397eb2020c203ea5f1426b4a42c2ede93c51
Vicenticoo <vicente.soto.torres@alumnos.cmch.maristas.cl>
2024-11-22 10:07:32 -0300

    Se suben los cambios  github

---
+++ script3.sh
@@ -0,0 +1,8 @@
+if git branch --list "test" > /dev/null; then
+    echo "La rama ya existe"
+    echo "Cambiando a la rama..."
+    git switch test
+else
+    git switch -c test
+    echo "Rama creada con éxito"
+fi
